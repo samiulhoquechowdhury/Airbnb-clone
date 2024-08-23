8fe77a885ec383eac5cc6c54a329f080a5656696
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-23 23:22:35 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className='text-3xl text-red-600 bg-slate-600 p-2 m-0 text-center'>Hello World</h1>
+      Hello World
+    </div>
+  )
+}
+
+export default App
