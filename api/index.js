6d35cf5cb09c8d1b798074b5db0bc028e305aee8
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-07 23:48:24 +0530

    test

--- api/index.js
+++ api/index.js
@@ -14,13 +14,36 @@ console.log(process.env.MONGO_URL)
 const MONGO_URL = process.env.MONGO_URL
 
 
+// testig
 
-app.get('/test', (req, res) => {
+app.get('/api/test', (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
     res.json('test ok');
-});
-
-app.post('/register', (req, res) => {
-    const { name, email, Password } = req.body;
-    res.json({ name, email, Password })
 })
+
+
+
+app.post('/api/register', async (req, res) => {
+    mongoose.connect(process.env.MONGO_URL);
+    const { name, email, password } = req.body;
+  
+    try {
+      const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt),
+      });
+      res.json(userDoc);
+    } catch (e) {
+      res.status(422).json(e);
+    }
+  
+  });
+
+
+
+
+
+
+
 app.listen(4000);
